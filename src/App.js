c7f72c216f46eb8c955b8bc4ab65a6607e5a8568
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-05-06 11:20:51 +0530

    Update App.js
    CSSTransition use

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import {Transition} from 'react-transition-group';
+import {CSSTransition} from 'react-transition-group';
 import './App.css';
 
 
@@ -28,21 +28,21 @@ class App extends Component{
       <div className='App'>
         <button onClick={() => !this.setState({show: !this.state.show})}>Toggle</button>
 
-      <Transition in={this.state.show} timeout={animateTime} mountOnEnter unmountOnExit onEnter={() => console.log('onEnter')} onEntering={() => console.log('onEntering')} onEntereed={() => console.log('onEntered')} onExit={() => console.log('onExit')} onExited={() => console.log('onExited')} onExiting={() => console.log('onExiting')}>
+      <CSSTransition in={this.state.show} timeout={animateTime} mountOnEnter unmountOnExit onEnter={() => console.log('onEnter')} onEntering={() => console.log('onEntering')} onEntereed={() => console.log('onEntered')} onExit={() => console.log('onExit')} onExited={() => console.log('onExited')} onExiting={() => console.log('onExiting')}>
           {
             //mode => (<p>{mode}</p>)
             mode => (
               <div style={{
                 background: 'black',
                 color: 'white',
-                transition: 'opacity 1s ease-out',
-                opacity: mode === 'exited' || mode === 'entering' ? 0 : 1
-              }}>
+                //transition: 'opacity 1s ease-out',
+                //opacity: mode === 'exited' || mode === 'entering' ? 0 : 1
+              }} className='my-class'>
               <h2>I am some text!</h2>
             </div>
             )
           }
-      </Transition>
+      </CSSTransition>
 
       <h2>I am outside transition!</h2>
         
