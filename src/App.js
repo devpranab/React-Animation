d90c856d37cce3aef3b1d70d6f25e831a8293dc5
pranav s <dev.pranabsk@gmail.com>
2022-05-06 11:10:40 +0530

    Timing and Event

--- src/App.js
+++ src/App.js
@@ -19,25 +19,32 @@ class App extends Component{
     //exiting
     //exited
 
+    const animateTime = {
+      enter: 100,
+      exit: 500
+    }
+
     return (
       <div className='App'>
         <button onClick={() => !this.setState({show: !this.state.show})}>Toggle</button>
 
-      <Transition in={this.state.show} timeout={2000}>
+      <Transition in={this.state.show} timeout={animateTime} mountOnEnter unmountOnExit onEnter={() => console.log('onEnter')} onEntering={() => console.log('onEntering')} onEntereed={() => console.log('onEntered')} onExit={() => console.log('onExit')} onExited={() => console.log('onExited')} onExiting={() => console.log('onExiting')}>
           {
             //mode => (<p>{mode}</p>)
             mode => (
               <div style={{
                 background: 'black',
                 color: 'white',
-                transition: 'opacity 2s ease-out',
-                opacity: mode === 'exited' ? 0 : 1
+                transition: 'opacity 1s ease-out',
+                opacity: mode === 'exited' || mode === 'entering' ? 0 : 1
               }}>
               <h2>I am some text!</h2>
             </div>
             )
           }
       </Transition>
+
+      <h2>I am outside transition!</h2>
         
       </div>
     );
