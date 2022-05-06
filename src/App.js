2ad9b95be28355251b8eb1aae7cd1292ba9910ff
pranav s <dev.pranabsk@gmail.com>
2022-05-06 10:59:55 +0530

    Using Transition Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import {Transition} from 'react-transition-group';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  state = {
+       show: true
+  }
+  render() {
+    // const style = {
+    //   //display: this.state.show ? 'block' : 'none',
+    //   background: 'black',
+    //   color: 'white'
+    // }
+
+    //entering
+    //entered
+    //exiting
+    //exited
+
+    return (
+      <div className='App'>
+        <button onClick={() => !this.setState({show: !this.state.show})}>Toggle</button>
+
+      <Transition in={this.state.show} timeout={2000}>
+          {
+            //mode => (<p>{mode}</p>)
+            mode => (
+              <div style={{
+                background: 'black',
+                color: 'white',
+                transition: 'opacity 2s ease-out',
+                opacity: mode === 'exited' ? 0 : 1
+              }}>
+              <h2>I am some text!</h2>
+            </div>
+            )
+          }
+      </Transition>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
